c669849bae023d77a18900ea8ce76ca8a2efef4e
Abdul Aziz <bintangkapala@gmail.com>
2024-12-21 14:14:11 +0700

    set up project

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`);
+});
