f8d62a1aa5de72f9462935870ec492871254460d
Abdul Aziz <bintangkapala@gmail.com>
2024-12-22 22:14:41 +0700

    chore(import): delete 'Request' imported from express

--- src/controllers/orderController.ts
+++ src/controllers/orderController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { Response } from "express";
 import asyncHandler from "../middlewares/asyncHandlerMiddleware";
 import prisma from "../config/prismaClient";
 import { AuthenticatedRequest } from "../types/AuthenticatedRequestType";
