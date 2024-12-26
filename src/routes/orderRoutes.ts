bfcd1d11fdec10fd05c7ac3c3c2de3959559031a
Abdul Aziz <bintangkapala@gmail.com>
2024-12-26 14:31:22 +0700

    feat(API): add history order route

--- src/routes/orderRoutes.ts
+++ src/routes/orderRoutes.ts
@@ -1,11 +1,12 @@
 import express from "express";
-import { checkout, trackOrder, updateOrderStatus, getAllOrders } from "../controllers/orderController";
+import { checkout, trackOrder, updateOrderStatus, getAllOrders, ordersHistory } from "../controllers/orderController";
 import { authenticate, authorizeRoles } from "../middlewares/authMiddleware";
 
 const router = express.Router();
 
 router.post("/checkout", authenticate, checkout);
 router.get("/track", authenticate, trackOrder);
+router.get("/history", authenticate, ordersHistory);
 router.get("/", authenticate, authorizeRoles(["SELLER, ADMIN"]), getAllOrders);
 router.put("/:id", authenticate, authorizeRoles(["SELLER"]), updateOrderStatus);
 
